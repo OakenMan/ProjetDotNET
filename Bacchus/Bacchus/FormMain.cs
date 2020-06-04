98ca34428fa6809851f1ccc0cc314d2949ddcf6b
OakenMan <tom.suchel@gmail.com>
2020-06-04 12:29:05 +0200

    First commit (début UI)

---
+++ Bacchus/Bacchus/FormMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bacchus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Change la taille minimum de la section gauche (la TreeView)
+            splitContainer1.Panel1MinSize = 200;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
